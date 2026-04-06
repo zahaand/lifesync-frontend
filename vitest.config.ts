2fd50b4ee2146fa5b54cebda6e0d97769103365f
a.zakharenkov <a.zakharenkov@mkskom.ru>
2026-04-06 20:33:18 +0300

    fix: исправление ошибок TypeScript в vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import * as path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
